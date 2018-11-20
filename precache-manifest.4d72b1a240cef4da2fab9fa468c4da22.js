4d471e26267cc54d313f913b008868d3123dcb6b
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2018-11-20 17:31:38 -0200

    Build

---
+++ precache-manifest.4d72b1a240cef4da2fab9fa468c4da22.js
@@ -0,0 +1,52 @@
+self.__precacheManifest = [
+  {
+    "url": "/23ed3e846743e0c4b1b07828c6d8bae0.jpg"
+  },
+  {
+    "revision": "c67c5a8c3786db580664",
+    "url": "/main.a44bef1f.chunk.css"
+  },
+  {
+    "revision": "d53d57e405a7cf0b07ab",
+    "url": "/runtime~main.d53d57e4.js"
+  },
+  {
+    "revision": "c67c5a8c3786db580664",
+    "url": "/main.c67c5a8c.js"
+  },
+  {
+    "revision": "f5055a5ff37138163be01989c7bdd3d2",
+    "url": "/index.html"
+  },
+  {
+    "url": "/fc630ab66248b71fa48116d24715e578.jpg"
+  },
+  {
+    "revision": "57105720f4e4cda0083d8beb31c49631",
+    "url": "/favicon.ico"
+  },
+  {
+    "url": "/f9bfeaeafa7e3b9b37156b6d827c960a.jpg"
+  },
+  {
+    "revision": "56f235d5f4fab04a08e5aa8397e228d7",
+    "url": "/bundle-loading.gif"
+  },
+  {
+    "url": "/66989d6cb1d3db8ed3df4279d136f433.jpg"
+  },
+  {
+    "url": "/634ae1a21dba5c2b318a6ee85d8fd6d3.jpg"
+  },
+  {
+    "url": "/4f176d6aaa86c4a40b2bd961c3e60f44.jpg"
+  },
+  {
+    "revision": "849415b76f02cd0cfd80",
+    "url": "/2.849415b7.js"
+  },
+  {
+    "revision": "849415b76f02cd0cfd80",
+    "url": "/2.1d31f053.chunk.css"
+  }
+];
